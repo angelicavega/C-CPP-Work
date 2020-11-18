0a78a13153d119cbdc9567f339bf08c18a90d653
Angélica M. Vega Aponte <av3387a@student.american.edu>
2020-11-17 22:55:04 -0500

    Sample c++ robot code for basic setup

---
+++ RobotDemo.cpp
@@ -0,0 +1,19 @@
+#include "WPILib.h"
+
+class RobotDemo : public SimpleRobot
+{
+public:
+	RobotDemo(void)
+	{
+			//initialization code goes here
+	}
+	void Autonomous(void)
+	{
+			//autonomous code goes here
+	}
+	void OperatorControl(void)
+	{
+			//teleop code goes here
+	}
+};
+START ROBOT CLASS(RobotDemo);
